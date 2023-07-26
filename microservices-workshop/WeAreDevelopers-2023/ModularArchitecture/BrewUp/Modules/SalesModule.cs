338998c83860ae72cca610392d4b628aa5f6cfa0
De Bock Benny <bdebock@protonmail.com>
2023-07-26 17:20:46 +0200

    Add salesmodule

---
+++ microservices-workshop/WeAreDevelopers-2023/ModularArchitecture/BrewUp/Modules/SalesModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BrewUp.Modules;
+
+public class SalesModule : IModule
+{
+	public SalesModule()
+	{
+	}
+
+    public bool IsEnabled => throw new NotImplementedException();
+
+    public int Order => throw new NotImplementedException();
+
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IServiceCollection RegisterModule(WebApplicationBuilder builder)
+    {
+
+        throw new NotImplementedException();
+    }
+}
+

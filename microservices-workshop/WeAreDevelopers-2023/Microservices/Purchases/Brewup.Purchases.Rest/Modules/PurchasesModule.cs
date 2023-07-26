17b0ccbc87530c463a3d2841ab48a3800c62f915
De Bock Benny <bdebock@protonmail.com>
2023-07-26 17:10:52 +0200

    commit new repo

---
+++ microservices-workshop/WeAreDevelopers-2023/Microservices/Purchases/Brewup.Purchases.Rest/Modules/PurchasesModule.cs
@@ -0,0 +1,40 @@
+﻿using Brewup.Purchases.ApplicationService;
+using Brewup.Purchases.ApplicationService.Endpoints;
+
+namespace Brewup.Purchases.Rest.Modules;
+
+public class PurchasesModule : IModule
+{
+	public bool IsEnabled => true;
+	public int Order => 20;
+
+	public IServiceCollection RegisterModule(WebApplicationBuilder builder)
+	{
+		builder.Services.AddPurchases();
+
+		return builder.Services;
+	}
+
+	public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+	{
+		var group = endpoints.MapGroup("/api/v1/")
+			.WithTags("Purchases");
+
+		group.MapGet("/", () => Results.Ok())
+			.Produces(StatusCodes.Status400BadRequest)
+			.Produces(StatusCodes.Status200OK)
+			.WithName("GetOrders");
+
+		group.MapPost("/Order", PurchasesEndpoints.HandleCreateOrder)
+			.Produces(StatusCodes.Status400BadRequest)
+			.Produces(StatusCodes.Status201Created)
+			.WithName("CreateOrder");
+
+		group.MapPut("/Order/{id}/complete", PurchasesEndpoints.HandleSetOrderStatusToComplete)
+			.Produces(StatusCodes.Status400BadRequest)
+			.Produces(StatusCodes.Status202Accepted)
+			.WithName("SetStatusToComplete");
+
+		return endpoints;
+	}
+}
---
+++ microservices-workshop/WeAreDevelopers-2023/Microservices/Warehouses/BrewUp.Warehouses.ApplicationServices/IWarehouseFacade.cs
@@ -0,0 +1,9 @@
+﻿using BrewUp.Warehouses.ReadModel.Entities;
+using BrewUp.Warehouses.SharedKernel.Dtos;
+
+namespace BrewUp.Warehouses.ApplicationServices;
+
+public interface IWarehouseFacade
+{
+	Task<PagedResult<BeerJson>> GetBeerAvailabilityAsync(CancellationToken cancellationToken);
+}

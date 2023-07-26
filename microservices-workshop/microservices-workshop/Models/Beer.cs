51382351b58028336254dca9171af2080139c84a
MichielSaey <36725454+MichielSaeyHogent@users.noreply.github.com>
2023-07-26 16:35:18 +0200

    Models added

---
+++ microservices-workshop/microservices-workshop/Models/Beer.cs
@@ -0,0 +1,17 @@
+﻿namespace microservices_workshop.Controllers;
+
+public class Beer
+{
+    public string Name { get; set; }
+    
+    public string? Brewery { get; set; }
+    
+    public string? Country { get; set; }
+    
+    public double Price { get; set; }
+    
+    public double Abv { get; set; }
+    
+    public string? Description { get; set; }
+    
+}
---
+++ microservices-workshop/microservices-workshop/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace microservices_workshop.Controllers;
+
+public class Order
+{
+
+    public Order()
+    {
+        OrderDate = DateTime.Now;
+    }
+    public int Id { get; set; }
+    // Metadata    
+    public string? CustomerName { get; set; }
+    public string? CustomerAddress { get; set; }
+    
+    // Order details
+    
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal Total { get; set; }
+    public DateTime OrderDate { get; set; }
+}
